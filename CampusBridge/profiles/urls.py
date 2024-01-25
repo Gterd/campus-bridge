a7feb0174865798899cad8510bf9e2d9a5b24731
Gterd <kighofose@gmail.com>
2024-01-25 17:04:28 +0100

    test

--- CampusBridge/profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.userprofile, name='userprofile'),
-    path('vendor/', views.vendorprofile, name='vendorprofile')
-]
--- CampusBridge/service/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Service, Service_Category, College
-
-admin.site.register(Service_Category)
-admin.site.register(College)
-admin.site.register([Service])
